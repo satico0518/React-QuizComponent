3af0168fe0644372fc2e2d1da2c09327359c8ff8
DavoDev <davo.gomez1@gmail.com>
2020-09-09 19:00:08 -0500

    fixing state

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require('./quiz_data.json');
 class Quiz extends Component {
   constructor(props) {
     super(props);
-    this.state = { quizPosition: 1 }
+    this.state = { quiz_position: 1 }
   }
   render() {
   return <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>;
